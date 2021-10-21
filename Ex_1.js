94c2d338529aa80b9c153fae4774ea787e54c940
HaSahakyan <92768238+HaSahakyan@users.noreply.github.com>
2021-10-21 13:19:45 +0400

    Create Ex_1.js

---
+++ Ex_1.js
@@ -0,0 +1,13 @@
+let heightRectangle = 7;
+let widthRectangle = 4;
+let areaRectangle = heightRectangle * widthRectangle;
+let heightTriangle = 7;
+let baseTriangle = 4;
+let areaTriangle = (heightTriangle * baseTriangle) / 2;
+
+if (areaRectangle > areaTriangle){
+   console.log(“The rectangle has a bigger area”);
+}
+else {
+  console.log(“The triangle has a bigger area”);
+}
