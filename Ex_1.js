9af55c34b5330480703f7b328af61c07be53d29a
HaSahakyan <92768238+HaSahakyan@users.noreply.github.com>
2021-10-21 13:07:01 +0400

    Create Ex_1.js

---
+++ Ex_1.js
@@ -0,0 +1,8 @@
+let X1 = 5;
+let Y1 = -6;
+let X2 = 4;
+let Y2 = 2;
+
+let length = Math.sqrt(((X2 - X1) **2) + ((Y2- Y1) **2))
+
+console.log("The length between (" + X1 + ", " + Y1+ ") and (" + X2+ ", " + Y2 + ") is " + length)
