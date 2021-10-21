e1b88e9c31713c50eac8cffef47b907f29ac5fa3
HaSahakyan <92768238+HaSahakyan@users.noreply.github.com>
2021-10-21 13:01:22 +0400

    Create Ex_2.js

---
+++ Ex_2.js
@@ -0,0 +1,15 @@
+let a = 7;
+let b = 3;
+let c = 6;
+let discriminant = (b **2) + (4 * a * c);
+if (discriminant > 0) {
+    let X1 = (0 - b + Math.sqet(discriminant)) / (2 * a);
+    let X2 = (0 - b - Math.sqet(discriminant)) / (2 * a);
+    console.log("X1 = " + X1);
+    console.log("X2 = " + X2);
+}if (discriminant = 0) {
+    let X = (0 - b) / (2 * a);
+    console.log("X = " + X);
+} else {
+    console.log("Unsolvable")
+}
